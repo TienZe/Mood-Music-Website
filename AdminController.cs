b981839f7b06c0318e01661c1ad666dbd027e7be
longvnhue <126792164+longvnhue@users.noreply.github.com>
2023-05-16 22:01:18 +0700

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PBL3.Models.Domain;
+using PBL3.Models.DTO;
+
+namespace PBL3.Controllers
+{
+    public class AdminController : Controller
+    {
+        public readonly AppDbContext context;
+        public static AppUser stateDetail;
+        //[Authorize]
+        public IActionResult UserDetail()
+        {
+            
+            return View(TestDetail.Instance.GetAll());
+        }
+
+        //[HttpGet]
+        //public IActionResult Detail()
+        //{
+        //   // if (id == String.Empty || id.Length == 0) return NotFound();
+
+        //    //AppUser user = listuser.FirstOrDefault(x => x.Id == id);
+
+        //    return View();
+        //}
+
+        public IActionResult Detail(string id)
+        {
+            stateDetail = TestDetail.Instance.GetAll().SingleOrDefault(p => p.Id == id);
+            return View(stateDetail);
+        }
+
+       
+
+        
+    }
+}
---
+++ TestDetail.cs
@@ -0,0 +1,34 @@
+﻿using PBL3.Models.Domain;
+
+namespace PBL3.Models.DTO
+{
+    public class TestDetail
+    {
+        private List<AppUser> A;
+        private static TestDetail _instance;
+
+        public static TestDetail Instance { get { if (_instance == null) _instance = new TestDetail(); return _instance; } private set => _instance = value; }
+        
+        public TestDetail() { 
+            A = new List<AppUser>();
+            for (int i = 1; i <= 100; i++)
+            {
+                A.Add(new AppUser
+                {
+                    Id = i.ToString(),
+                    Name = "Đặng Hoài Phương",
+                    UserName = "Phuong1234",
+                    Birthday = new DateTime(2000, 12, 31),
+                    Email = "phuongitf@dut.udn.vn",
+                    Gender = Gender.Male,
+                    PhoneNumber = "999988880"
+                });
+            }
+        }
+
+        public List<AppUser> GetAll()
+        {
+            return A;
+        }
+    }
+}
