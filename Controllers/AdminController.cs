1c0b959159b5eea464cd360f57528c1636710aaf
longvnhue <hj3u126@gmail.com>
2023-05-28 23:40:53 +0700

    Upload layout trang Admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PBL3.Controllers
+{
+    public class AdminController : Controller
+    {
+        //[Authorize(Roles = Models.Domain.Role.Admin)]
+        public IActionResult MemberDetail()
+        {
+            return View();
+        }
+    }
+}
