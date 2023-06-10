8814cb0517435b78bb2bed77162aa70fd0e47ba2
Ciel Tan <ngoduytan21072003@gmail.com>
2023-06-10 14:37:30 +0700

    update trang list member

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -98,5 +98,28 @@ namespace PBL3.Controllers
                 StoryImage = order.Story.AvatarImage
             });
         }
+
+        public async Task<IActionResult> ListMember(int? pageIndex, string searchString)
+        {
+            // Server validation
+            pageIndex = (pageIndex == null || pageIndex < 1) ? 1 : pageIndex;
+
+            IEnumerable<AppUser> listMember = await userService.GetUsersInRoleAsync(Role.Member);
+            // Tiếp tục lọc (in-memory)
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                // Thực hiện filter theo emotion name
+                listMember = listMember.Where(member => (member.Name?.Contains(searchString) ?? false)
+                    || member.Email.Contains(searchString));
+            }
+            // Truyền filter hiện tại sang cho View
+            ViewBag.SearchString = searchString ?? string.Empty;
+
+            // Sắp xếp
+            listMember = listMember.OrderBy(member => member.Id);
+
+            // Phân trang kết quả
+            return View(PaginatedList<AppUser>.CreateAsync(listMember, pageIndex.Value, PageSize));
+        }
     }
 }
